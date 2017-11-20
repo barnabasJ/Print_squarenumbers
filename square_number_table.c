ed89b4983cbd9c256537f713d625c706b86fee93
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2017-11-20 07:53:47 +0100

    added format options to printf

--- square_number_table.c
+++ square_number_table.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
- 
-int main()
-{
-	int number=1;
-	int squared=0;
 
-	printf("Enter a number to show all squares from 1 up until the number\n");
-	scanf("%d",&squared);
+int main() {
+  int number = 1;
+  int squared = 0;
 
-	int limit=squared+1;
+  printf("Enter a number to show all squares from 1 up until the number\n");
+  scanf("%d", &squared);
 
-	printf("n | n^2\n");
-	printf("-------------\n");
-	while(number<limit) {
-	squared=number*number;
-	printf("%d | %d\n", number, squared);
-	number++;
-	}
-	
-return(0);
+  int limit = squared + 1;
+
+  printf("n  | n^2\n");
+  printf("-------------\n");
+  while (number < limit) {
+    squared = number * number;
+    printf("%-2d | %d\n", number, squared);
+    number++;
+  }
+
+  return (0);
 }
